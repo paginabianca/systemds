7d53577234c14bab2b2dd245ba5dd8461c709344
Andreas Botzner <andreas@botzner.com>
2022-08-17 16:33:32 +0200

    [timing] regression search

--- src/main/java/org/apache/sysds/runtime/instructions/fed/MultiReturnParameterizedBuiltinFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/MultiReturnParameterizedBuiltinFEDInstruction.java
@@ -358,6 +358,7 @@ public class MultiReturnParameterizedBuiltinFEDInstruction extends ComputationFE
 			// build necessary structures for encoding
             // NOTE: in this build of the MultiColumnEncoder there are a bunch of
             Timing t1 = new Timing(true);
+            LOG.debug("THIS LINE SHOULD NOT BE IN LOCAL EXECUTION");
             if(ConfigurationManager.getDMLConfig().getBooleanValue(DMLConfig.FEDERATED_PAR_TRANSFORMENCODE)){
               LOG.info("building with " +OptimizerUtils.getTransformNumThreads() +" threads");
               encoder.build(fb, OptimizerUtils.getTransformNumThreads() ); // FIXME skip equi-height sorting
